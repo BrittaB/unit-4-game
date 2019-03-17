d89105dae940e4ad43356b142a56706d8c5745e1
Britta <polished.athena@gmail.com>
2019-03-17 16:59:01 -0500

    java updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,10 +2,16 @@
 var goal = 0;
 var wins = 0;
 var losses = 0;
-var totalScore = 0;
+var guessesRight = true;
+var guessesWrong = false;
+var crystalBlu= 0;
+var crystalPur= 0;
+var crystalRed= 0;
+var crystalYel= 0;
+var totalScore = crystalBlu + crystalPur + crystalRed + crystalYel;
 
-//write a function with a random number for each crystal
-$(document).ready(function() {
+//write a function with a random number for goal total
+$(document).ready(function randomNumber() {
     $("#random-button").on("click", function() {
       var random = Math.floor(Math.random() * 1000) + 1;
       $("#random-number").text(random);
@@ -14,7 +20,10 @@ $(document).ready(function() {
 //write a function that puts the total number of all of the crystals within the #goal placement
 
 //write a function that creates buttons for each of the crystals
-
+$(document).ready(function(){
+  $("img").click(function(){
+    $(this).hide();
+  });
 //write a function that lists the totalScore of the crystal bottons pressed.
 
 //write a function that totals out the number of wins 
